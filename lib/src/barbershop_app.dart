cf692a935b1972020454aa9b47aa7153767a0f2b
Felipe Sales <soufeliposales@gmail.com>
2023-08-15 21:51:27 -0300

    style: add lints rules

---
+++ lib/src/barbershop_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BarbershopApp extends StatelessWidget {
+  const BarbershopApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(''),
+        ),
+        body: Container(),
+      ),
+    );
+  }
+}

3e5bf91e328849c09110583b37a6da635de86772
Felipe Sales <soufeliposales@gmail.com>
2023-09-09 13:46:37 -0300

    refactor: enhance UserModel as expected

--- lib/src/models/user_model.dart
+++ lib/src/models/user_model.dart
@@ -1,4 +1,11 @@
 sealed class UserModel {
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    this.avatar,
+  });
+
   factory UserModel.fromMap(Map<String, dynamic> json) {
     return switch (json['profile']) {
       'ADM' => UserModelADM.fromMap(json),
@@ -7,40 +14,12 @@ sealed class UserModel {
     };
   }
 
-  const UserModel({
-    required this.id,
-    required this.name,
-    required this.email,
-    this.avatar,
-  });
-
   final int id;
   final String name, email;
   final String? avatar;
 }
 
-final class UserModelADM extends UserModel {
-  factory UserModelADM.fromMap(Map<String, dynamic> json) {
-    return switch (json) {
-      {
-        'id': final int id,
-        'name': final String name,
-        'email': final String email,
-        'work_days': final List workDays,
-        'work_hours': final List workHours,
-      } =>
-        UserModelADM(
-          id: id,
-          name: name,
-          email: email,
-          avatar: json['avatar'] as String?,
-          workDays: workDays.cast<String>(),
-          workHours: workHours.cast<int>(),
-        ),
-      _ => throw FormatException('Invalid UserModelADM JSON: $json'),
-    };
-  }
-
+class UserModelADM extends UserModel {
   UserModelADM({
     required super.id,
     required super.name,
@@ -50,11 +29,42 @@ final class UserModelADM extends UserModel {
     this.workHours,
   });
 
+  factory UserModelADM.fromMap(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        'id': final int id,
+        'name': final String name,
+        'email': final String email,
+      } =>
+        UserModelADM(
+          id: id,
+          name: name,
+          email: email,
+          avatar: json['avatar'],
+          // ignore: avoid_dynamic_calls
+          workDays: json['work_days']?.cast<String>(),
+          // ignore: avoid_dynamic_calls
+          workHours: json['work_hours']?.cast<int>(),
+        ),
+      _ => throw ArgumentError('Invalid Json'),
+    };
+  }
+
   final List<String>? workDays;
   final List<int>? workHours;
 }
 
 class UserModelEmployee extends UserModel {
+  UserModelEmployee({
+    required super.id,
+    required super.name,
+    required super.email,
+    required this.barbershopId,
+    super.avatar,
+    required this.workDays,
+    required this.workHours,
+  });
+
   factory UserModelEmployee.fromMap(Map<String, dynamic> json) {
     return switch (json) {
       {
@@ -74,19 +84,9 @@ class UserModelEmployee extends UserModel {
           avatar: json['avatar'],
           barbershopId: barbershopId,
         ),
-      _ => throw FormatException('Invalid UserModelEmployee JSON: $json'),
+      _ => throw ArgumentError('Invalid UserModelEmployee JSON: $json'),
     };
   }
-
-  UserModelEmployee({
-    required super.id,
-    required super.name,
-    required super.email,
-    super.avatar,
-    required this.workDays,
-    required this.workHours,
-    required this.barbershopId,
-  });
 
   final List<String> workDays;
   final List<int> workHours;
